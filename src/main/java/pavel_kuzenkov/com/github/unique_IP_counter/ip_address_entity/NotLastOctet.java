530a5b1cad5c577b53adff9082b61901e8fda848
PavelKuzenkov <paul_501@mail.ru>
2019-12-01 23:23:24 +0400

    Поправил NotLastOctet. Добавил конструктор с параметрами. Теперь октет при создании занет, предпоследний он или нет. Теперь проблема при добавлении следующих октетов. Придется каждый раз проверять. Лучшеб завел ещё один класс, типа PreLastOctet.

--- src/main/java/pavel_kuzenkov/com/github/unique_IP_counter/ip_address_entity/NotLastOctet.java
+++ src/main/java/pavel_kuzenkov/com/github/unique_IP_counter/ip_address_entity/NotLastOctet.java
@@ -13,7 +13,7 @@ class NotLastOctet implements Octet {
     /**
      * Массив со следующими октетами.
      */
-    private Octet[] nextOctets = new Octet[256];
+    private Octet[] nextOctets = null;
 
     /**
      * Индикатор заполненности массива со следующими октетами.
@@ -25,13 +25,39 @@ class NotLastOctet implements Octet {
      */
     private short fillCounter = 0;
 
+    /**
+     * Конструктор с параметрами. Принимает порядковый номер создаваемого октета.
+     * Если октет предпоследний (третий) - то нужно инициализировать массив классом LastOctet.
+     * @param octetNumber номер создаваемого октета.
+     */
+    public NotLastOctet(int octetNumber) {
+        if (octetNumber < 3) {
+            nextOctets = new NotLastOctet[256];
+        } else {
+            nextOctets = new LastOctet[256];
+        }
+    }
+
+    /**
+     * Проверка данного октета на заполненность.
+     * @return true если данный и следующий октет заполнены(содержит все возможные варианты),
+     * false если октет ещё не заполнен.
+     */
     @Override
     public boolean isFull() {
         return full;
     }
 
+    /**
+     * Добавление нового значения октета. Учет заполненности октета.
+     * @param address массив со значениями октетов.
+     * @param octetNumber номер ячейки массива, содержащей значение для следующего октета.
+     * @return true если адрес уникален(в хранилище ещё нет такого адреса) и был сохран в хранилище,
+     * false если хранилище уже содержит такой адрес.
+     */
     @Override
     public boolean addNextOctet(short[] address, int octetNumber) {
+        //TODO сам себя загнал в тупик) Как теперь понять, какой октет следующий? Каждый раз проверять?
         return false;
     }
 }
