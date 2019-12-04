8ce54c87bae75f190f711f7a2ce805b6af32055c
pavelkuzenkov <pavel.kuzenkov@dz.ru>
2019-12-04 18:29:00 +0400

    Переименовал FileReader в IPAddressFileReader

---
+++ src/main/java/pavel_kuzenkov/com/github/unique_IP_counter/file_reader/IPAddressFileReader.java
@@ -0,0 +1,11 @@
+package pavel_kuzenkov.com.github.unique_IP_counter.file_reader;
+
+/**
+ * Class IPAddressFileReader. Класс для открытия текстового файла и чтения из него IP-адресов.
+ *
+ * @author Kuzenkov Pavel.
+ * @since 01.12.2019
+ */
+public class IPAddressFileReader {
+
+}
