b844acab356b694eb93609085363587a38f0af6e
pavelkuzenkov <pavel.kuzenkov@dz.ru>
2019-12-04 13:23:20 +0400

    Реализовал ленивую инициализацию в UniqueAddressLazyRepository

--- src/main/java/pavel_kuzenkov/com/github/unique_IP_counter/ip_address_entity/UniqueAddressLazyRepository.java
+++ src/main/java/pavel_kuzenkov/com/github/unique_IP_counter/ip_address_entity/UniqueAddressLazyRepository.java
@@ -12,7 +12,7 @@ package pavel_kuzenkov.com.github.unique_IP_counter.ip_address_entity;
  * в которой хранится ссылка на октет. Значением второго октета - номер ячейки массива в самом октете и т.д.
  * до последнего октета. Предпологается, что когда в хранилище будут присутствовать
  * все возможные адреса для какой либо подсети(например все хосты сети 192.168.1.0/24) - то соответствующие ветви дерева
- * "схлопнуться".
+ * "схлопнуться". Входящие адреса проверяются на валидность. Если адрес не валиден - в консоль пишется сообщение.
  *
  * @author Kuzenkov Pavel.
  * @since 01.12.2019
@@ -21,7 +21,10 @@ package pavel_kuzenkov.com.github.unique_IP_counter.ip_address_entity;
 class UniqueAddressLazyRepository implements AddressRepository {
 
     /**
-     * Блоки "ленивой" инициализации. Содержат уникальные адреса
+     * Блоки "ленивой" инициализации. Содержат последовательности октетов (уникальных адресов).
+     * Данные массивы являются первыми в последовательности. Фактически являются "корнем дерева".
+     * Списки первых октетов.
+     * Инициализируются массивами по 64 ячейки каждый, вместо одного массива на 256 ячеек.
      */
     private Octet[] addressesBlock1 = null;
     private Octet[] addressesBlock2 = null;
@@ -36,85 +39,155 @@ class UniqueAddressLazyRepository implements AddressRepository {
     /**
      * Индикаторы заполненности блоков "ленивой" инициализации.
      */
-    private boolean block1full = false;
-    private boolean block2full = false;
-    private boolean block3full = false;
-    private boolean block4full = false;
+    private boolean[] blocksFull = new boolean[4];
 
     /**
      * Счетчики заполненности блоков "ленивой" инициализации.
      */
-    private byte fillCounter1 = 0;
-    private byte fillCounter2 = 0;
-    private byte fillCounter3 = 0;
-    private byte fillCounter4 = 0;
+    private byte[] fillCounters = new byte[4]; //По идее так должно уходить чуть меньше памяти, чем с четыремя отдельными переменными. Даже не смотря на накладные расходы на массив.
 
 //    /**
 //     * Счетчик уникальных адресов.
 //     */
 //    private long uniqueAddressCounter = 0;
 
-    @Override
-    public boolean put(int[] incomingAddress) {
-        return true;
-//        whichBlock(incomingAddress[0]);
-//        int position = 0;
-//        Octet destination = addresses[incomingAddress[0]];
-//        if (destination == null) {
-//            return createNewOctetAndPut(incomingAddress);
-//        }
-//        if (destination.isFull()) return false;
-//        return destination.addNextOctet(incomingAddress, 1);
-//    }
-//
-//    private int whichBlock(short firstOctetValue) {
-//        if (firstOctetValue >= 0 && firstOctetValue < 64) return 1;
-//        if (firstOctetValue >= 64 && firstOctetValue < 128) return 2;
-//        if (firstOctetValue >= 128 && firstOctetValue < 192) return 3;
-//        if (firstOctetValue >= 192 && firstOctetValue < 256) return 4;
-    }
-
-//    /**
-//     * Инициализация нового октета и запись в него нового значения.
-//     * @param incomingAddress Массив значений октетов IP-адреса.
-//     * @return true т.к. адрес уникален(новый октет) и был сохран в хранилище.
-//     */
-//    private boolean createNewOctetAndPut(short[] incomingAddress) {
-//        NotLastOctet newOctet = new NotLastOctet();
-//        addresses[incomingAddress[0]] = newOctet;
-//        return newOctet.addNextOctet(incomingAddress, 1);
-//    }
-
-
-
+    /**
+     * Проверка хранилища на заполненность.
+     * @return true если все 4 блока "ленивой" инициализации заполнены,
+     * т.е. хранилище заполнено (содержит все возможные варианты IP-адресов),
+     * false если хранилище ещё не заполнено.
+     */
     @Override
     public boolean isFull() {
         return full;
     }
 
-//    @Override
-//    public long getNumberOfUniqueAddresses() {
-//        return uniqueAddressCounter;
-//    }
+    /**
+     * Добавление нового адреса в хранилище. Учет заполненности хранилища. Сначала находим блок "ленивой"
+     * инициализации, куда нужно записать новыйадрес. Делаем это путем деления нацело
+     * значения первого октета на 64 (количестко ячеек в каждом блоке), Проверяем, не заполнен ли уже
+     * этот блок. Затем находим номер ячейки в этом блоке, куда необходимо записать новое значение.
+     * Делаем это нахождением остатка от деления значения первого октета на 64. Если ячейка не занята - создаём
+     * новый Octet и передаём ему массив с адресом. Если занята - проверяем не заполнен ли следующий
+     * октет и передаем массив с адресом. После этого проверяем, не заполнился ли октет после
+     * добавления в него новой записи. Если заполнился - увеличиваем счетчик соответствующего блока.
+     * Если счетчик равен 64(блок заполнился) выставляем соотвествующий флаг и проверяем и удаляем
+     * (схлопываем) блок. Все следующие попытки записи в этот блок будут пресекаться. Если заполнены
+     * все 4 блока - выставляем флаг заполненности у всего хранилища.
+     *
+     * @param incomingAddress массив со значениями октетов входящего адреса.
+     * @return true если адрес уникален(в хранилище ещё нет такого адреса) и был сохран в хранилище,
+     * false если хранилище уже содержит такой адрес.
+     */
+    // Возможно код не совсем понятен при чтении, из-за обилия "хардкода". Не стал выводить в
+    // отдельные методы и заводить переменные т.к. экземпляров этого класса может создаться очень
+    // много.
+    @Override
+    public boolean put(int[] incomingAddress) {
+        if (!isValidAddress(incomingAddress)) {
+            printInfo(incomingAddress);
+            return false;
+        }
+        if (blocksFull[incomingAddress[0] / 64]) return false; //Если блок уже был заполнен - сразу выходим. Такой адрес уже есть в хранилище.
+        Octet[] destinationBlock = getDestinationBlock(incomingAddress[0]);
+        boolean result = false;
+        Octet destinationOctet = destinationBlock[incomingAddress[0] % 64]; //Т.к. 4 массива по 64 ячейки, чтобы узнать место в массиве назначения, нужно взять остаток от деления.
+        if (destinationOctet == null) {
+            return createNewOctetAndAdd(destinationBlock, incomingAddress);
+        }
+        if (!destinationOctet.isFull()) {
+            result = destinationOctet.addNextOctet(incomingAddress);
+            if (destinationOctet.isFull()) { //Возможно после добавления, следующий октет заполнился.
+                if ((fillCounters[incomingAddress[0] / 64] = ++fillCounters[incomingAddress[0] / 64]) >= 64) {//В принципе можно объеденить в один if.
+                    blocksFull[incomingAddress[0] / 64] = true;
+                    deleteNeededBlock(incomingAddress[0] / 64); //В данный блок пришли все возможные значения. Начинаем освобождать память (схлопывать ветвь).
+                    full = (blocksFull[0] && blocksFull[1] && blocksFull[2] && blocksFull[3]); //Если заполнены все 4 блока - значит в октет пришли все возможные значения.
+                }
+            }
+        }
+        return result;
+    }
 
     /**
-     * Инициализация нужного блока с адресами.
-     * @param blocNumber номер блока, который необходимо инициализировать.
+     * Определяем, с каким блоком "ленивой" инициализации предстоит работать.
+     * Если нужный блок еще не инициализирован - он проинициализируется.
+     * Метод не проверяет, был ли блок заполнен, поэтому вызывается только после проверки на заполненность.
+     * @param octetValue Значение октета.
+     * @return нужный нам блок.
      */
-    private void initiateOctetsBlock(int blocNumber) {
+    private Octet[] getDestinationBlock(int octetValue) {
+        if (octetValue >= 0 && octetValue < 64) {
+            return addressesBlock1 != null ? addressesBlock1 : (addressesBlock1 = new Octet[64]);
+        }
+        if (octetValue >= 64 && octetValue < 128) {
+            return addressesBlock2 != null ? addressesBlock2 : (addressesBlock2 = new Octet[64]);
+        }
+        if (octetValue >= 128 && octetValue < 192) {
+            return addressesBlock3 != null ? addressesBlock3 : (addressesBlock3 = new Octet[64]);
+        }
+        return addressesBlock4 != null ? addressesBlock4 : (addressesBlock4 = new Octet[64]); //Не стал делать ещё одну проверку потому что по идее другие значения в хранилище прийти не могут(проверка на входе хранилища)
+    }
+
+    /**
+     * Инициализация нового октета и запись в него нового значения.
+     * @param address Массив значений октетов IP-адреса.
+     * @return true т.к. адрес уникален(новый октет) и был сохран в хранилище.
+     */
+    private boolean createNewOctetAndAdd(Octet[] destinationBlock, int[] address) {
+        LazySecondOctet newOctet = new LazySecondOctet();
+        destinationBlock[address[1] % 64] = newOctet;
+        return newOctet.addNextOctet(address);
+    }
+
+    /**
+     * Удаление заполненного блока со значениями первого октета.
+     * @param blocNumber номер блока, который необходимо удалить.
+     */
+    private void deleteNeededBlock(int blocNumber) {
         switch (blocNumber) {
+            case 0:
+                addressesBlock1 = null;
+                break;
             case 1:
-                addressesBlock1 = new Octet[64];
+                addressesBlock2 = null;
                 break;
             case 2:
-                addressesBlock2 = new Octet[64];
+                addressesBlock3 = null;
                 break;
             case 3:
-                addressesBlock3 = new Octet[64];
-                break;
-            case 4:
-                addressesBlock4 = new Octet[64];
+                addressesBlock4 = null;
                 break;
         }
+    }
+
+    /**
+     * Проверка входящего массива с IP-адресом на валидность. По идее данный метод должен быть тут, так как
+     * хранилище должно само проверять входные данные на валидность. Хотя, мы теряем немного в производительности.
+     * @param incomingAddress Массив значений октетов IP-адреса.
+     * @return true если адрес валидный, false если адрес не валидный.
+     */
+    private boolean isValidAddress(int[] incomingAddress) {
+        if (incomingAddress.length != 4) {
+            return false;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (incomingAddress[i] < 0 || incomingAddress[i] > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * вывод в консоль сообщения о поступлении невалидного адреса.
+     * @param incomingAddress Массив значений октетов IP-адреса.
+     */
+    private void printInfo(int[] incomingAddress) {
+        StringBuilder message = new StringBuilder();
+        for (int address : incomingAddress) {
+            message.append(address).append(".");
+        }
+        message.append(" - невалидный IP-адрес!");
+        System.out.println(message);
     }
 }
